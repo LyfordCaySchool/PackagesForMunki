91b9b7c5d5d973e5791d0f076bb9dac2114576f2
Paul Suh <paul.suh@ps-enable.com>
2016-08-07 17:37:54 -0400

    Initial import of sub-packages

---
+++ LCIS-NETWORK config profile/postflight-staff.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/usr/bin/profiles -I -F /private/tmp/LCIS-NETWORK-Staff.mobileconfig
+/bin/rm /private/tmp/LCIS-NETWORK-Staff.mobileconfig
---
+++ TrustProfileForLCIS/postinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/usr/bin/profiles -I -F /private/tmp/Trust_Profile_for_Lyford_Cay_International_School.mobileconfig
+/bin/rm /private/tmp/Trust_Profile_for_Lyford_Cay_International_School.mobileconfig
