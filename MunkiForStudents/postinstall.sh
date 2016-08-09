c99a2b9a09de6d24a2550394448c756a1372de86
Paul Suh <paul.suh@ps-enable.com>
2016-08-09 11:40:33 -0400

    MunkiForStudents added image and intro text
    Also added additional defaults write to postinstall script so that
    Munki will handle Apple software updates

--- MunkiForStudents/postinstall.sh
+++ MunkiForStudents/postinstall.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 /usr/bin/defaults write /Library/Preferences/ManagedInstalls.plist \
-	ClientIdentifier LCIS_Student_2016-17
+	ClientIdentifier "LCIS Student 2016-17"
 /usr/bin/defaults write /Library/Preferences/ManagedInstalls.plist \
 	SoftwareRepoURL "https://munki.lyfordcayschool.org/"
-
+/usr/bin/defaults write /Library/Preferences/ManagedInstalls.plist \
+	InstallAppleSoftwareUpdates -bool True
